89d98124359b0cfe50ac6d8d2ff4ab2b8dca1bf7
hutao <hutao@szkingdom.com>
2020-10-28 23:25:34 +0800

    httpclient demo

---
+++ Week_01/demo/src/LearnBytecode.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @program: Week_01
+ * @description: 字节码学习
+ * @author: hutao
+ * @create: 2020-10-21 21:01
+ */
+public class LearnBytecode {
+}
---
+++ Week_02/nio/src/main/java/com/hutoo/nio/HttpClientDemo.java
@@ -0,0 +1,10 @@
+package com.hutoo.nio;
+
+/**
+ * @program: Week_02
+ * @description: HttpClient demo
+ * @author: hutao
+ * @create: 2020-10-28 23:17
+ */
+public class HttpClientDemo {
+}
